f78fd39aed9cb2f522c03f4efccdf0732e3e9e4b
Godefroy <courtin.godefroy@gmail.com>
2020-10-21 18:07:36 +0200

    fin de la quête

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+    <div>
+        <nav className="Navbar">
+            <a href="https://fr.wikipedia.org/wiki/Les_Simpson" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/7/79/Les_simpson_logo_France.png/1280px-Les_simpson_logo_France.png" width="150" height="75" class="d-inline-block align-top" alt="" />
+            </a>
+        </nav>
+    </div>
+)
+
+export default Navbar;
