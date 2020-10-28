4c7d75d57c2b3234d2d42371dd574da0ae8c5755
Godefroy <courtin.godefroy@gmail.com>
2020-10-28 13:06:47 +0100

    ajout form ok

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './QuoteForm.css'
+
+
 
 const QuoteForm = () => {
 
+    const [title, setTitle] = useState("")
+    const [characters, setCharacters] = useState("")
+
+    const changeTitle = (event) => {
+        // console.log("state :", event.target.value)
+        setTitle(event.target.value)
+    }
+    // const charactersRemaining = (event) => {
+    //     // console.log("state :", event.target.value)
+    //     setCharacters(event.target.value)
+    // }
+
+
     return (
         <div className="QuoteForm">
-            <h3>titre</h3>
-            <label htmlFor="character">Character</label>
-            <input id="character" name="character" type="text" />
+            <h3>You typed: {title.toUpperCase()}</h3>
+            <label htmlFor="character">Character:</label>
+            <input onChange={changeTitle} id="character" name="character" type="text" value={title} />
+            {/* <input onChange={charactersRemaining} id="character" name="character" type="text" value={characters} />
+            <p>{10 - characters.length}</p> */}
+            <p>characters remaining: {20 - title.length}</p>
         </div>
     )
 }
